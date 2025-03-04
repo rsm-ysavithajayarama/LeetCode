6208f46271e210af201fe04b1519630d4bd4b908
Yaksha <141078295+rsm-ysavithajayarama@users.noreply.github.com>
2025-03-03 19:35:51 -0800

    Time: 12 ms (54.20%) | Memory: 18 MB (49.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mydict1, mydict2 = {}, {}
-        for i in s:
-            if i in mydict1:
-                mydict1[i] +=1
-            else:
-                mydict1[i] = 1
-        
-        for j in t:
-            if j in mydict2:
-                mydict2[j] +=1
-            else:
-                mydict2[j] = 1
-        
-        return mydict1 == mydict2
-        
+        def counting(array):
+            mydict = {}
+            for i in array:
+                if i in mydict:
+                    mydict[i]+=1
+                else:
+                    mydict[i] = 1
+            return mydict
+
+
+        return counting(s) == counting(t)
